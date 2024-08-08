dbd67d3e96c242cf6579acbf5ed09061f2b52659
CCreech <ccreech@airway.com>
2024-08-08 16:24:56 -0400

    Create TrakkerController.cs

---
+++ AirwayAPI/Controllers/TrakkerControllers/TrakkerController.cs
@@ -0,0 +1,30 @@
+﻿using AirwayAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirwayAPI.Controllers.TrakkerControllers
+{
+    public class TrakkerController : ControllerBase
+    {
+        private readonly eHelpDeskContext _context;
+
+        public TrakkerController(eHelpDeskContext context)
+        {
+            _context = context;
+        }
+
+        
+        [HttpGet("Companies")]
+        public async Task<IActionResult> GetCompanies()
+        {
+            var companies = await _context.TrkCompanies
+                .Where(c => c.CompanyId != "MNS")
+                .OrderBy(c => c.CompanyId)
+                .Select(c => new { c.CompanyId, c.CompanyName })
+                .ToListAsync();
+
+            return Ok(companies);
+        }
+        
+    }
+}
