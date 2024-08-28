bc9c907691e5e2edde9474c7b271d2f2e94329d0
CCreech <ccreech@airway.com>
2024-08-28 10:07:06 -0400

    Create TrkRwvendor.cs
    Scaffold trkRWVendors table.

---
+++ AirwayAPI/Models/TrkRwvendor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirwayAPI.Models;
+
+public partial class TrkRwvendor
+{
+    public string? VendorNum { get; set; }
+
+    public string? VendorName { get; set; }
+
+    public string? CompanyId { get; set; }
+
+    public string? Address1 { get; set; }
+
+    public string? Address2 { get; set; }
+
+    public string? Address3 { get; set; }
+
+    public string? Address4 { get; set; }
+
+    public string? Text2 { get; set; }
+
+    public string? City { get; set; }
+
+    public string? State { get; set; }
+
+    public string? Zip { get; set; }
+
+    public string? Country { get; set; }
+}
