fa5609dd29873840f84d1f61c83f58f6734a33af
CCreech <ccreech@airway.com>
2024-09-05 14:50:48 -0400

    Create AuthenticationResult.cs

---
+++ AirwayAPI/Models/SecurityModels/AuthenticationResult.cs
@@ -0,0 +1,9 @@
+﻿namespace AirwayAPI.Models.SecurityModels
+{
+    public class AuthenticationResult
+    {
+        public bool IsSuccess { get; private set; }
+        public static AuthenticationResult Success() => new() { IsSuccess = true };
+        public static AuthenticationResult Failure() => new() { IsSuccess = false };
+    }
+}
