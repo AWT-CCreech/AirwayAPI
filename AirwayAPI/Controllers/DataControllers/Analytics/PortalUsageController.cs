839e4852b91278343eae4c67fc34047d81023a3e
CCreech <ccreech@airway.com>
2024-10-01 10:12:25 -0400

    Update directory layout
    Move PortalUsageController to Analytics subdirectory of DataControllers directory

--- AirwayAPI/Controllers/DataControllers/Analytics/PortalUsageController.cs
+++ AirwayAPI/Controllers/DataControllers/Analytics/PortalUsageController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace AirwayAPI.Controllers.DataControllers
+namespace AirwayAPI.Controllers.DataControllers.Analytics
 {
     [Authorize]
     [ApiController]
