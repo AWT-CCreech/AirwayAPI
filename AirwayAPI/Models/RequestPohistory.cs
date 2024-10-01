12ecc4d1f866fe9fe05731ba3756240d20892c6b
CCreech <ccreech@airway.com>
2024-10-01 10:16:27 -0400

    Create RequestPohistory.cs

---
+++ AirwayAPI/Models/RequestPohistory.cs
@@ -0,0 +1,18 @@
+﻿namespace AirwayAPI.Models;
+
+public partial class RequestPohistory
+{
+    public int Id { get; set; }
+
+    public int? Poid { get; set; }
+
+    public string? Ponum { get; set; }
+
+    public DateTime? DeliveryDate { get; set; }
+
+    public int? QtyBought { get; set; }
+
+    public int? EnteredBy { get; set; }
+
+    public DateTime? EditDate { get; set; }
+}
