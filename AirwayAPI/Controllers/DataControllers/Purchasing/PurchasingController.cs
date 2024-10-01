446cb0312cd8a0dc5e12b0c7e0465a85f4716dca
CCreech <ccreech@airway.com>
2024-10-01 10:10:56 -0400

    Update directory layout
    Move PurchasingController to Purchasing subdirectory of DataControllers directory

--- AirwayAPI/Controllers/DataControllers/Purchasing/PurchasingController.cs
+++ AirwayAPI/Controllers/DataControllers/Purchasing/PurchasingController.cs
@@ -2,10 +2,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace AirwayAPI.Controllers.DataControllers
+namespace AirwayAPI.Controllers.DataControllers.Purchasing
 {
     [Authorize]
     [ApiController]
