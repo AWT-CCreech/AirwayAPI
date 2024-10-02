2704327bd93838cbfc91d7ab68bf2eac0832a422
CCreech <ccreech@airway.com>
2024-10-02 16:06:16 -0400

    Create PODetailEmailInput.cs

---
+++ AirwayAPI/Models/PODeliveryLogModels/PODetailEmailInput.cs
@@ -0,0 +1,17 @@
+﻿namespace AirwayAPI.Models.PODeliveryLogModels
+{
+    public class PODetailEmailInput
+    {
+        public string FromEmail { get; set; } = "purch_dept@airway.com"; // Default email
+        public string ToEmail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool Urgent { get; set; }
+        public string SalesOrderNum { get; set; }
+        public string CompanyName { get; set; }
+        public string SalesRequiredDate { get; set; }
+        public string DeliveryDate { get; set; }
+        public string PartNumber { get; set; }
+        public string Notes { get; set; }
+    }
+}
