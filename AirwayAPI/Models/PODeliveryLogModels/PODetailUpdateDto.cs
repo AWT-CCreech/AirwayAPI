ab52892545d7ef2cb9f5bf31a2902b5f65dd19e1
CCreech <ccreech@airway.com>
2024-10-07 11:20:52 -0400

    Update PODetailUpdateDto.cs
    - Updated Notes, Password, QtyOrdered, QtyReceived, and ReceiverNum properties to be nullable for enhanced flexibility.
    - Added NewNote and NotesList properties for capturing additional note details.
    - Ensured alignment with frontend model by making necessary fields optional and adjusting data types where appropriate.

--- AirwayAPI/Models/PODeliveryLogModels/PODetailUpdateDto.cs
+++ AirwayAPI/Models/PODeliveryLogModels/PODetailUpdateDto.cs
@@ -3,16 +3,20 @@
     public class PODetailUpdateDto
     {
         public int Id { get; set; }
-        public string? Notes { get; set; } // Make Notes nullable
+        public string? NewNote { get; set; } 
         public DateTime? ExpectedDelivery { get; set; }
-        public int? UserId { get; set; }
+        public int? UserId { get; set; } 
         public string UserName { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; } 
         public int ContactID { get; set; }
         public string PONum { get; set; }
-        public string SONum {  get; set; }
+        public string SONum { get; set; }
         public string PartNum { get; set; }
         public bool UpdateAllDates { get; set; }
         public bool UrgentEmail { get; set; }
+        public int? QtyOrdered { get; set; }
+        public int? QtyReceived { get; set; }
+        public int? ReceiverNum { get; set; }
+        public List<string> NotesList { get; set; } 
     }
 }
