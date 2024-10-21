5b6443ebd457f69b19a9f4245e9e84ca02d5a071
CCreech <ccreech@airway.com>
2024-10-21 15:52:26 -0400

    Update PODeliveryLogController.cs
    Remove unnecessary using statement

--- AirwayAPI/Controllers/ReportControllers/PODeliveryLogControllers/PODeliveryLogController.cs
+++ AirwayAPI/Controllers/ReportControllers/PODeliveryLogControllers/PODeliveryLogController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace AirwayAPI.Controllers.ReportControllers
 {
