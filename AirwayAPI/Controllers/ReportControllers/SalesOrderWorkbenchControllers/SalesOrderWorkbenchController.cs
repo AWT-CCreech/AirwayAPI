8e0eea5a9967d03f2696904963fe8e1669ab7b85
AWT-CCreech <ccreech@airway.com>
2024-10-25 13:45:41 -0400

    Create SalesOrderWorkbenchController.cs

---
+++ AirwayAPI/Controllers/ReportControllers/SalesOrderWorkbenchControllers/SalesOrderWorkbenchController.cs
@@ -0,0 +1,21 @@
+﻿using AirwayAPI.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirwayAPI.Controllers.ReportControllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SalesOrderWorkbenchController : ControllerBase
+    {
+        private readonly eHelpDeskContext _context;
+
+        public SalesOrderWorkbenchController(eHelpDeskContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
