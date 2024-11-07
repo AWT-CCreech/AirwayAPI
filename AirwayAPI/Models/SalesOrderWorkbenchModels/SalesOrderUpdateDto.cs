ac6701b4e0b4854bb30dcdbfc3c014fae74bd0a6
CCreech <ccreech@airway.com>
2024-11-07 08:41:10 -0500

    Create SalesOrderUpdateDto.cs

---
+++ AirwayAPI/Models/SalesOrderWorkbenchModels/SalesOrderUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿namespace AirwayAPI.Models.SalesOrderWorkbenchModels
+{
+    public class SalesOrderUpdateDto
+    {
+        public int SaleId { get; set; }
+        public string RWSalesOrderNum { get; set; }
+        public bool DropShipment { get; set; }
+        public int EventId { get; set; }
+        public int QuoteId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Subject { get; set; }
+        public string HtmlBody { get; set; }
+    }
+}
