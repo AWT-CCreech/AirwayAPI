1ba70151a65445b12667ae87dd7163d6e610e97a
CCreech <ccreech@airway.com>
2024-11-07 08:41:14 -0500

    Create QtSalesOrderDetail.cs

---
+++ AirwayAPI/Models/QtSalesOrderDetail.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirwayAPI.Models;
+
+public partial class QtSalesOrderDetail
+{
+    public int Id { get; set; }
+
+    public int? SaleId { get; set; }
+
+    public int? RequestId { get; set; }
+
+    public int? SaleOrder { get; set; }
+
+    public int? QuoteQty { get; set; }
+
+    public int? QtySold { get; set; }
+
+    public string? UnitMeasure { get; set; }
+
+    public string? PartNum { get; set; }
+
+    public string? PartDesc { get; set; }
+
+    public double? UnitPrice { get; set; }
+
+    public double? ExtendedPrice { get; set; }
+
+    public decimal? CurUnitPrice { get; set; }
+
+    public decimal? CurExtPrice { get; set; }
+
+    public bool? AutoSoflag { get; set; }
+
+    public bool? UpdatedByAutoSo { get; set; }
+
+    public bool? Soflag { get; set; }
+}
