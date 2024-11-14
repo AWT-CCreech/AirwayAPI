587b2a86e5ddb3ff5542b55243ed254005133a00
CCreech <ccreech@airway.com>
2024-11-14 15:44:28 -0500

    Update EquipmentRequestUpdateDto.cs
    - Added `DropShipment` property to the EquipmentRequestUpdateDto model for updating EquipmentRequests

--- AirwayAPI/Models/SalesOrderWorkbenchModels/EquipmentRequestUpdateDto.cs
+++ AirwayAPI/Models/SalesOrderWorkbenchModels/EquipmentRequestUpdateDto.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string RWSalesOrderNum { get; set; } = string.Empty;
+        public bool DropShipment { get; set; } = false;
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string Subject { get; set; } = string.Empty;
