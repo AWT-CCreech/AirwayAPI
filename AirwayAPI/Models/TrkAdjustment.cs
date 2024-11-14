7e5078dbb993550db3ebec4ded08cc8e39245cd0
CCreech <ccreech@airway.com>
2024-11-14 15:44:46 -0500

    Create TrkAdjustment.cs
    - Scaffolded via EF

---
+++ AirwayAPI/Models/TrkAdjustment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirwayAPI.Models;
+
+public partial class TrkAdjustment
+{
+    public int Id { get; set; }
+
+    public string? ItemNum { get; set; }
+
+    public int? QtyAdjustment { get; set; }
+
+    public string? Reason { get; set; }
+
+    public DateTime? EntryDate { get; set; }
+
+    public int? EnteredBy { get; set; }
+
+    public string? ScanGroup { get; set; }
+}
