d52ac76fe2aac0a1e65192e6f1ae80ccac95636b
CCreech <ccreech@airway.com>
2024-11-14 15:44:54 -0500

    Create TrkSoldItemHistory.cs
    - Scaffolded via EF

---
+++ AirwayAPI/Models/TrkSoldItemHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirwayAPI.Models;
+
+public partial class TrkSoldItemHistory
+{
+    public int RowId { get; set; }
+
+    public int? RequestId { get; set; }
+
+    public DateTime? DateMarkedBought { get; set; }
+
+    public string? Username { get; set; }
+
+    public string? PageName { get; set; }
+}
