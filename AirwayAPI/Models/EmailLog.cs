1ded38ccfd9e09e99df3e452f7334f3b2a14d8a7
CCreech <ccreech@airway.com>
2024-11-19 16:05:22 -0500

    Create EmailLog.cs
    Scaffolded from eHelpDesk

---
+++ AirwayAPI/Models/EmailLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirwayAPI.Models;
+
+public partial class EmailLog
+{
+    public int Id { get; set; }
+
+    public string Subject { get; set; } = null!;
+
+    public string Body { get; set; } = null!;
+
+    public string OrderType { get; set; } = null!;
+
+    public string OrderNumber { get; set; } = null!;
+
+    public string SenderEmail { get; set; } = null!;
+
+    public string? Recipients { get; set; }
+
+    public string LoggedBy { get; set; } = null!;
+
+    public DateTime? LoggedAt { get; set; } = DateTime.UtcNow;
+}
