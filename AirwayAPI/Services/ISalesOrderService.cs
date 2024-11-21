801cb8e36e0b8eb45adeff3ddfa4d6e072499eaa
CCreech <ccreech@airway.com>
2024-11-21 16:03:29 -0500

    Create ISalesOrderService.cs

---
+++ AirwayAPI/Services/ISalesOrderService.cs
@@ -0,0 +1,10 @@
+﻿using AirwayAPI.Models.DTOs;
+using System.Threading.Tasks;
+
+namespace AirwayAPI.Services
+{
+    public interface ISalesOrderService
+    {
+        Task UpdateSalesOrderAsync(SalesOrderUpdateDto request);
+    }
+}
