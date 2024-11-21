0ab893b6ad35c1c6749e9d7e173d1812c29e983e
CCreech <ccreech@airway.com>
2024-11-21 16:03:33 -0500

    Create ITokenService.cs

---
+++ AirwayAPI/Services/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace AirwayAPI.Services
+{
+    public interface ITokenService
+    {
+        string GenerateJwtToken(string username);
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+    }
+}
