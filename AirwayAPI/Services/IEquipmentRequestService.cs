c5dd73cce3f112b3d317bfe0c839ee7dd9868b33
CCreech <ccreech@airway.com>
2024-11-21 16:03:25 -0500

    Create IEquipmentRequestService.cs

---
+++ AirwayAPI/Services/IEquipmentRequestService.cs
@@ -0,0 +1,12 @@
+﻿using AirwayAPI.Models.DTOs;
+using AirwayAPI.Models;
+using System.Threading.Tasks;
+
+namespace AirwayAPI.Services
+{
+    public interface IEquipmentRequestService
+    {
+        Task ProcessEquipmentRequest(QtSalesOrderDetail detail, SalesOrderUpdateDto request);
+        Task<QtSalesOrderDetail> GetSalesOrderDetailByIdAsync(int id);
+    }
+}
