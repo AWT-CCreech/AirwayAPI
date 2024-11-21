2d5a5bee28b08841f3b78fe39c4e09c766640268
CCreech <ccreech@airway.com>
2024-11-21 16:03:09 -0500

    Create EquipmentRequestUpdateDto.cs

---
+++ AirwayAPI/Models/DTOs/EquipmentRequestUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AirwayAPI.Models.DTOs
+{
+    public class EquipmentRequestUpdateDto
+    {
+        public int Id { get; set; }
+        public string RWSalesOrderNum { get; set; } = string.Empty;
+        public bool DropShipment { get; set; }
+        public string Username { get; set; } = string.Empty;
+    }
+}
