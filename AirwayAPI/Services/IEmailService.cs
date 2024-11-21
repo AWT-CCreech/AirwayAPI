a4b04760c3381d9878473408a6f5eac4918e35ec
CCreech <ccreech@airway.com>
2024-11-21 16:03:21 -0500

    Create IEmailService.cs

---
+++ AirwayAPI/Services/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using AirwayAPI.Models.DTOs;
+using System.Threading.Tasks;
+
+namespace AirwayAPI.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(EmailInputDto emailInput);
+    }
+}
