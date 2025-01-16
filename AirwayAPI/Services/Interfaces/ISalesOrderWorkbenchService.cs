ead5144a60ea94910446a5cbfc03cdceeac38999
CCreech <ccreech@airway.com>
2025-01-16 16:17:39 -0500

    Create ISalesOrderWorkbenchService.cs

---
+++ AirwayAPI/Services/Interfaces/ISalesOrderWorkbenchService.cs
@@ -0,0 +1,8 @@
+﻿namespace AirwayAPI.Services.Interfaces
+{
+    public interface ISalesOrderWorkbenchService
+    {
+        Task<List<object>> GetEventLevelDataAsync(int? salesRepId, string? billToCompany, int? eventId);
+        Task<List<object>> GetDetailLevelDataAsync(int? salesRepId, string? billToCompany, int? eventId);
+    }
+}
