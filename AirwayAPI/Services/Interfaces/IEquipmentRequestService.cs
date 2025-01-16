4b39b168e73fec9c159443c5ce64c8206087be4f
CCreech <ccreech@airway.com>
2025-01-16 16:20:13 -0500

    Update IEquipmentRequestService.cs

--- AirwayAPI/Services/Interfaces/IEquipmentRequestService.cs
+++ AirwayAPI/Services/Interfaces/IEquipmentRequestService.cs
@@ -7,6 +7,6 @@ namespace AirwayAPI.Services.Interfaces
     public interface IEquipmentRequestService
     {
         Task ProcessEquipmentRequest(QtSalesOrderDetail detail, SalesOrderUpdateDto request);
-        Task<QtSalesOrderDetail> GetSalesOrderDetailByIdAsync(int id);
+        Task UpdateEquipmentRequestAsync(EquipmentRequestUpdateDto request);
     }
 }
