3d9f26e3918ee74182d39231d016d58a265bed50
CCreech <ccreech@airway.com>
2025-01-16 16:19:18 -0500

    Update SalesOrderUpdateDto.cs

--- AirwayAPI/Models/DTOs/SalesOrderUpdateDto.cs
+++ AirwayAPI/Models/DTOs/SalesOrderUpdateDto.cs
@@ -5,8 +5,8 @@
         public int SaleId { get; set; }
         public int EventId { get; set; }
         public int QuoteId { get; set; }
-        public string RWSalesOrderNum { get; set; } = string.Empty;
-        public bool DropShipment { get; set; }
+        public string SalesOrderNum { get; set; } = string.Empty;
+        public bool DropShipment { get; set; } = false;
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string Subject { get; set; } = string.Empty;
