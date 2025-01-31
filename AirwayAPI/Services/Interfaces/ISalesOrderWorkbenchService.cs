36a71cbdc17aaeb6cf97ebd9413dff8390f050ec
CCreech <ccreech@airway.com>
2025-01-31 16:15:57 -0500

    Update ISalesOrderWorkbenchService.cs

--- AirwayAPI/Services/Interfaces/ISalesOrderWorkbenchService.cs
+++ AirwayAPI/Services/Interfaces/ISalesOrderWorkbenchService.cs
@@ -7,7 +7,7 @@ namespace AirwayAPI.Services.Interfaces
         Task<List<object>> GetEventLevelDataAsync(int? salesRepId, string? billToCompany, int? eventId);
         Task<List<object>> GetDetailLevelDataAsync(int? salesRepId, string? billToCompany, int? eventId);
 
-        Task UpdateEventLevelAsync(SalesOrderUpdateDto request);
-        Task UpdateDetailLevelAsync(EquipmentRequestUpdateDto request);
+        Task UpdateEventLevelAsync(EventLevelUpdateDto request);
+        Task UpdateDetailLevelAsync(DetailLevelUpdateDto request);
     }
 }
