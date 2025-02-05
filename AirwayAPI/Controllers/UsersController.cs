069678462c5b9a9af550237df67e5012358692ca
CCreech <ccreech@airway.com>
2025-02-05 14:29:13 -0500

    Create UsersController.cs

---
+++ AirwayAPI/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using AirwayAPI.Models;
+using AirwayAPI.Models.MassMailerModels;
+using AirwayAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AirwayAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController(IUserService userService) : ControllerBase
+    {
+        private readonly IUserService _userService = userService;
+
+        // GET: api/Users/active
+        [HttpGet("active")]
+        public async Task<ActionResult<IEnumerable<User>>> GetActiveUsers()
+        {
+            var users = await _userService.GetActiveUsersAsync();
+            return Ok(users);
+        }
+
+        // GET: api/Users/massmailer
+        [HttpGet("massmailer")]
+        public async Task<ActionResult<IEnumerable<MassMailerUser>>> GetMassMailerUsers()
+        {
+            var users = await _userService.GetMassMailerUsersAsync();
+            return Ok(users);
+        }
+    }
+}
