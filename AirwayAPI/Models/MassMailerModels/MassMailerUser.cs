068a30a68bec995f739c75a212b0adfb5d56091c
CCreech <ccreech@airway.com>
2025-02-05 16:05:07 -0500

    Update MassMailerUser.cs
    Include UserName as a prop in the MassMailerUser model

--- AirwayAPI/Models/MassMailerModels/MassMailerUser.cs
+++ AirwayAPI/Models/MassMailerModels/MassMailerUser.cs
@@ -7,6 +7,7 @@ namespace AirwayAPI.Models.MassMailerModels
 {
     public class MassMailerUser
     {
+        public string UserName { get; set; }
         public string Email { get; set; }
         public string FullName { get; set; }
     }
