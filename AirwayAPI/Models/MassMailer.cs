eea0ab2656e839a064719a39f6fa9479c0848ff3
CCreech <ccreech@airway.com>
2025-02-05 14:29:24 -0500

    Create MassMailer.cs

---
+++ AirwayAPI/Models/MassMailer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirwayAPI.Models;
+
+public partial class MassMailer
+{
+    public int MassMailId { get; set; }
+
+    public string? MassMailDesc { get; set; }
+
+    public DateTime? DateSent { get; set; }
+
+    public int? SentBy { get; set; }
+}
