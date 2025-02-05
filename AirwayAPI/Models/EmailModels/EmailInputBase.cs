8c4fb69aa5045245647c59487725222e2a6ed052
CCreech <ccreech@airway.com>
2025-02-05 16:04:10 -0500

    Update EmailInputBase.cs
    Simplify object declaration

--- AirwayAPI/Models/EmailModels/EmailInputBase.cs
+++ AirwayAPI/Models/EmailModels/EmailInputBase.cs
@@ -5,12 +5,12 @@
         public string? FromEmail { get; set; }       // Sender's email
         public string UserName { get; set; }         // SMTP username
         public string Password { get; set; }         // SMTP password
-        public List<string> ToEmails { get; set; } = new List<string>();  // List of recipient emails
+        public List<string> ToEmails { get; set; } = [];  // List of recipient emails
         public string Subject { get; set; }          // Email subject
         public string Body { get; set; }             // Email body
-        public List<string> CCEmails { get; set; } = new List<string>();  // List of CC emails
-        public List<string> Attachments { get; set; } = new List<string>(); // List of attachment file paths
-        public List<string> InlineImages { get; set; } = new List<string>(); // List of inline image file paths
+        public List<string> CCEmails { get; set; } = [];  // List of CC emails
+        public List<string> Attachments { get; set; } = []; // List of attachment file paths
+        public List<string> InlineImages { get; set; } = []; // List of inline image file paths
         public IDictionary<string, string>? Placeholders { get; set; } // List of placeholders tp be handled by EmailService
         public bool Urgent { get; set; }
     }
