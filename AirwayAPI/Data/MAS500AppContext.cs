274f098706bf53570e8282668cb5f0c57da12d92
CCreech <ccreech@airway.com>
2025-03-06 11:03:24 -0500

    Update MAS500AppContext.cs
    Rescaffold

--- AirwayAPI/Data/MAS500AppContext.cs
+++ AirwayAPI/Data/MAS500AppContext.cs
@@ -16,10 +16,130 @@ public partial class MAS500AppContext : DbContext
     {
     }
 
+    public virtual DbSet<TarCustomer> TarCustomers { get; set; }
+
     public virtual DbSet<TarInvoice> TarInvoices { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=AWTSQL19;Database=mas500_app;Trusted_Connection=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<TarCustomer>(entity =>
+        {
+            entity.HasKey(e => e.CustKey).HasName("PK__tarCusto__8CCAE2DB8E5CC056");
+
+            entity.ToTable("tarCustomer", tb =>
+                {
+                    tb.HasTrigger("tD_tarCustomer");
+                    tb.HasTrigger("tI_tarCustomer");
+                    tb.HasTrigger("tR_tarCustomer_AppAudit");
+                    tb.HasTrigger("tR_tarCustomer_DBAudit");
+                    tb.HasTrigger("tU_tarCustomer");
+                });
+
+            entity.HasIndex(e => new { e.CompanyId, e.CustId }, "XAK1tarCustomer").IsUnique();
+
+            entity.HasIndex(e => e.SalesSourceKey, "XIF102tarCustomer");
+
+            entity.HasIndex(e => e.DfltItemKey, "XIF239tarCustomer");
+
+            entity.HasIndex(e => e.NationalAcctLevelKey, "XIF255tarCustomer");
+
+            entity.HasIndex(e => e.CustClassKey, "XIF44tarCustomer");
+
+            entity.HasIndex(e => e.DfltSalesAcctKey, "XIF46tarCustomer");
+
+            entity.HasIndex(e => e.VendKey, "XIF47tarCustomer");
+
+            entity.HasIndex(e => e.StmtCycleKey, "XIF49tarCustomer");
+
+            entity.HasIndex(e => e.PrimaryAddrKey, "XIF50tarCustomer");
+
+            entity.HasIndex(e => e.DfltBillToAddrKey, "XIF51tarCustomer");
+
+            entity.HasIndex(e => e.DfltShipToAddrKey, "XIF52tarCustomer");
+
+            entity.HasIndex(e => e.StmtFormKey, "XIF53tarCustomer");
+
+            entity.HasIndex(e => e.PrimaryCntctKey, "XIF55tarCustomer");
+
+            entity.HasIndex(e => e.CurrExchSchdKey, "XIF56tarCustomer");
+
+            entity.HasIndex(e => e.DfltSalesReturnAcctKey, "XIF57tarCustomer");
+
+            entity.Property(e => e.CustKey).ValueGeneratedNever();
+            entity.Property(e => e.Abano)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasColumnName("ABANo");
+            entity.Property(e => e.BillingType).HasDefaultValue((short)1);
+            entity.Property(e => e.CompanyId)
+                .HasMaxLength(3)
+                .IsUnicode(false)
+                .HasColumnName("CompanyID");
+            entity.Property(e => e.CreateDate).HasColumnType("datetime");
+            entity.Property(e => e.CreateUserId)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("CreateUserID");
+            entity.Property(e => e.CreditLimit).HasColumnType("decimal(15, 3)");
+            entity.Property(e => e.CrmcustId)
+                .HasMaxLength(32)
+                .IsUnicode(false)
+                .HasColumnName("CRMCustID");
+            entity.Property(e => e.CustId)
+                .HasMaxLength(12)
+                .IsUnicode(false)
+                .HasColumnName("CustID");
+            entity.Property(e => e.CustName)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.CustRefNo)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.DateEstab).HasColumnType("datetime");
+            entity.Property(e => e.DfltMaxUpCharge).HasColumnType("decimal(15, 3)");
+            entity.Property(e => e.FinChgFlatAmt).HasColumnType("decimal(15, 3)");
+            entity.Property(e => e.FinChgPct)
+                .HasDefaultValue(0m)
+                .HasColumnType("decimal(5, 4)");
+            entity.Property(e => e.ReqCreditLimit)
+                .HasDefaultValue(0m)
+                .HasColumnType("decimal(15, 3)");
+            entity.Property(e => e.ReqPo).HasColumnName("ReqPO");
+            entity.Property(e => e.RetntPct)
+                .HasDefaultValue(0m)
+                .HasColumnType("decimal(5, 4)");
+            entity.Property(e => e.ShipPriority).HasDefaultValue((short)3);
+            entity.Property(e => e.Status).HasDefaultValue((short)1);
+            entity.Property(e => e.StdIndusCodeId)
+                .HasMaxLength(7)
+                .IsUnicode(false)
+                .HasColumnName("StdIndusCodeID");
+            entity.Property(e => e.TradeDiscPct)
+                .HasDefaultValue(0m)
+                .HasColumnType("decimal(5, 4)");
+            entity.Property(e => e.UpdateDate).HasColumnType("datetime");
+            entity.Property(e => e.UpdateUserId)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("UpdateUserID");
+            entity.Property(e => e.UserFld1)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.UserFld2)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.UserFld3)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.UserFld4)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+        });
+
         modelBuilder.Entity<TarInvoice>(entity =>
         {
             entity.HasKey(e => e.InvcKey).HasName("PK__tarInvoi__12691F6089BE8C75");
