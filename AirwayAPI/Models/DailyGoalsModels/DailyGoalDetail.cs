362fb60b23677c50af947547d2c907492b39e26e
CCreech <ccreech@airway.com>
2025-03-06 11:03:48 -0500

    Create DailyGoalDetail.cs
    Create model for retrieving daily goal details

---
+++ AirwayAPI/Models/DailyGoalsModels/DailyGoalDetail.cs
@@ -0,0 +1,12 @@
+﻿namespace AirwayAPI.Models.DailyGoalsModels
+{
+    public class DailyGoalDetail
+    {
+        public string OrderNum { get; set; }
+        public string CustomerName { get; set; }
+        public decimal QuoteTotal { get; set; }
+        // For "Shipped" records, you may return additional cost information:
+        public decimal? InvoiceCost { get; set; }
+        public decimal? ConsignCost { get; set; }
+    }
+}
