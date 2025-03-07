b132bec6636d51a3b1ad429c97882e8687cbb68d
CCreech <ccreech@airway.com>
2025-03-07 11:13:35 -0500

    Create CustomerPOSearchResult.cs

---
+++ AirwayAPI/Models/CustomerPOSearchModels/CustomerPOSearchResult.cs
@@ -0,0 +1,17 @@
+﻿namespace AirwayAPI.Models.CustomerPOSearchModels
+{
+    public class CustomerPOSearchResult
+    {
+        public string SoTranNo { get; set; }
+        public string CustPoNo { get; set; }
+        public DateTime SoTranDate { get; set; }
+        public decimal SoTranAmt { get; set; }
+        public string CustomerName { get; set; }
+        public string CustNum { get; set; }
+
+        // Values populated from the secondary query
+        public int EventID { get; set; }
+        public int SaleID { get; set; }
+        public int QuoteID { get; set; }
+    }
+}
