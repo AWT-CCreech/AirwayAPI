bb0f8dde63960c4ef756b4207f6ac231a4566632
CCreech <ccreech@airway.com>
2025-03-07 11:13:32 -0500

    Update MAS500AppContext.cs

--- AirwayAPI/Data/MAS500AppContext.cs
+++ AirwayAPI/Data/MAS500AppContext.cs
@@ -20,6 +20,8 @@ public partial class MAS500AppContext : DbContext
 
     public virtual DbSet<TarInvoice> TarInvoices { get; set; }
 
+    public virtual DbSet<TsoSalesOrder> TsoSalesOrders { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=AWTSQL19;Database=mas500_app;Trusted_Connection=True;TrustServerCertificate=True;");
@@ -272,6 +274,219 @@ public partial class MAS500AppContext : DbContext
                 .HasMaxLength(30)
                 .IsUnicode(false)
                 .HasColumnName("UpdateUserID");
+        });
+
+        modelBuilder.Entity<TsoSalesOrder>(entity =>
+        {
+            entity.HasKey(e => e.Sokey).HasName("PK__tsoSales__83F931CE092D58F9");
+
+            entity.ToTable("tsoSalesOrder", tb =>
+                {
+                    tb.HasTrigger("SalesOrderCreditHoldEmail");
+                    tb.HasTrigger("tD_tsoSalesOrder");
+                    tb.HasTrigger("tI_tsoSalesOrder");
+                    tb.HasTrigger("tR_tsoSalesOrder_DBAudit");
+                    tb.HasTrigger("tU_tsoSalesOrder");
+                });
+
+            entity.HasIndex(e => new { e.CompanyId, e.TranType, e.TranNoRel }, "XAK1tsoSalesOrder").IsUnique();
+
+            entity.HasIndex(e => new { e.BlnktSokey, e.CreateDate }, "XIE1tsoSalesOrder");
+
+            entity.HasIndex(e => new { e.CompanyId, e.TranType, e.TranNoRelChngOrd }, "XIE2tsoSalesOrder");
+
+            entity.HasIndex(e => e.Status, "XIE3tsoSalesOrder");
+
+            entity.HasIndex(e => e.StaxTranKey, "XIE4tsoSalesOrder");
+
+            entity.HasIndex(e => e.QuoteFormKey, "XIF1216tsoSalesOrder");
+
+            entity.HasIndex(e => e.CreditAuthUserId, "XIF2868tsoSalesOrder");
+
+            entity.HasIndex(e => e.CustQuoteKey, "XIF872tsoSalesOrder");
+
+            entity.HasIndex(e => e.RecurSokey, "XIF873tsoSalesOrder");
+
+            entity.HasIndex(e => e.CustKey, "XIF876tsoSalesOrder");
+
+            entity.HasIndex(e => e.PrimarySperKey, "XIF877tsoSalesOrder");
+
+            entity.HasIndex(e => e.CntctKey, "XIF879tsoSalesOrder");
+
+            entity.HasIndex(e => e.CustClassKey, "XIF880tsoSalesOrder");
+
+            entity.HasIndex(e => e.CurrId, "XIF881tsoSalesOrder");
+
+            entity.HasIndex(e => e.CurrExchSchdKey, "XIF882tsoSalesOrder");
+
+            entity.HasIndex(e => e.DfltAcctRefKey, "XIF883tsoSalesOrder");
+
+            entity.HasIndex(e => e.DfltShipZoneKey, "XIF884tsoSalesOrder");
+
+            entity.HasIndex(e => e.DfltShipMethKey, "XIF885tsoSalesOrder");
+
+            entity.HasIndex(e => e.DfltShipToAddrKey, "XIF886tsoSalesOrder");
+
+            entity.HasIndex(e => e.DfltShipToCaddrKey, "XIF887tsoSalesOrder");
+
+            entity.HasIndex(e => e.DfltWhseKey, "XIF888tsoSalesOrder");
+
+            entity.HasIndex(e => e.PmtTermsKey, "XIF892tsoSalesOrder");
+
+            entity.HasIndex(e => e.SoackFormKey, "XIF893tsoSalesOrder");
+
+            entity.HasIndex(e => e.BillToCustAddrKey, "XIF894tsoSalesOrder");
+
+            entity.HasIndex(e => e.TranType, "XIF896tsoSalesOrder");
+
+            entity.HasIndex(e => e.SalesSourceKey, "XIF897tsoSalesOrder");
+
+            entity.HasIndex(e => e.DfltPurchVaddrKey, "XIF900tsoSalesOrder");
+
+            entity.HasIndex(e => e.DfltVendKey, "XIF901tsoSalesOrder");
+
+            entity.HasIndex(e => e.BillToAddrKey, "XIF902tsoSalesOrder");
+
+            entity.Property(e => e.Sokey)
+                .ValueGeneratedNever()
+                .HasColumnName("SOKey");
+            entity.Property(e => e.AckDate).HasColumnType("datetime");
+            entity.Property(e => e.BlnktSokey).HasColumnName("BlnktSOKey");
+            entity.Property(e => e.CccustCode)
+                .HasMaxLength(17)
+                .IsUnicode(false)
+                .HasColumnName("CCCustCode");
+            entity.Property(e => e.ChngOrdDate).HasColumnType("datetime");
+            entity.Property(e => e.ChngReason)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.ChngUserId)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("ChngUserID");
+            entity.Property(e => e.CloseDate).HasColumnType("datetime");
+            entity.Property(e => e.CompanyId)
+                .HasMaxLength(3)
+                .IsUnicode(false)
+                .HasColumnName("CompanyID");
+            entity.Property(e => e.ConfirmNo)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.CreateDate).HasColumnType("datetime");
+            entity.Property(e => e.CreateUserId)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("CreateUserID");
+            entity.Property(e => e.CreditApprovedAmt).HasColumnType("decimal(15, 3)");
+            entity.Property(e => e.CreditAuthUserId)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("CreditAuthUserID");
+            entity.Property(e => e.CrmopportunityId)
+                .HasMaxLength(32)
+                .IsUnicode(false)
+                .HasColumnName("CRMOpportunityID");
+            entity.Property(e => e.CurrExchRate).HasDefaultValue(1.0);
+            entity.Property(e => e.CurrId)
+                .HasMaxLength(3)
+                .IsUnicode(false)
+                .HasColumnName("CurrID");
+            entity.Property(e => e.CustPono)
+                .HasMaxLength(15)
+                .IsUnicode(false)
+                .HasColumnName("CustPONo");
+            entity.Property(e => e.DfltCreatePo).HasColumnName("DfltCreatePO");
+            entity.Property(e => e.DfltDeliveryMeth).HasDefaultValue((short)1);
+            entity.Property(e => e.DfltFobkey).HasColumnName("DfltFOBKey");
+            entity.Property(e => e.DfltPromDate).HasColumnType("datetime");
+            entity.Property(e => e.DfltPurchVaddrKey).HasColumnName("DfltPurchVAddrKey");
+            entity.Property(e => e.DfltRequestDate).HasColumnType("datetime");
+            entity.Property(e => e.DfltShipDate).HasColumnType("datetime");
+            entity.Property(e => e.DfltShipPriority).HasDefaultValue((short)3);
+            entity.Property(e => e.DfltShipToCaddrKey).HasColumnName("DfltShipToCAddrKey");
+            entity.Property(e => e.DutyAmount).HasColumnType("decimal(15, 3)");
+            entity.Property(e => e.Expiration).HasColumnType("datetime");
+            entity.Property(e => e.FreightAmt).HasColumnType("decimal(15, 3)");
+            entity.Property(e => e.FreightMethod).HasDefaultValue((short)2);
+            entity.Property(e => e.HoldReason)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.NationalTaxAmount).HasColumnType("decimal(15, 3)");
+            entity.Property(e => e.NextLineNo).HasDefaultValue(1);
+            entity.Property(e => e.OpenAmt).HasColumnType("decimal(15, 3)");
+            entity.Property(e => e.OpenAmtHc)
+                .HasColumnType("decimal(15, 3)")
+                .HasColumnName("OpenAmtHC");
+            entity.Property(e => e.RecurSokey).HasColumnName("RecurSOKey");
+            entity.Property(e => e.RequireSoack).HasColumnName("RequireSOAck");
+            entity.Property(e => e.SalesAmt).HasColumnType("decimal(15, 3)");
+            entity.Property(e => e.SalesAmtHc)
+                .HasColumnType("decimal(15, 3)")
+                .HasColumnName("SalesAmtHC");
+            entity.Property(e => e.SoackFormKey).HasColumnName("SOAckFormKey");
+            entity.Property(e => e.StaxAmt)
+                .HasColumnType("decimal(15, 3)")
+                .HasColumnName("STaxAmt");
+            entity.Property(e => e.StaxCalc)
+                .HasDefaultValue((short)1)
+                .HasColumnName("STaxCalc");
+            entity.Property(e => e.StaxTranKey).HasColumnName("STaxTranKey");
+            entity.Property(e => e.TradeDiscAmt).HasColumnType("decimal(15, 3)");
+            entity.Property(e => e.TranAmt).HasColumnType("decimal(15, 3)");
+            entity.Property(e => e.TranAmtHc)
+                .HasColumnType("decimal(15, 3)")
+                .HasColumnName("TranAmtHC");
+            entity.Property(e => e.TranCmnt)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.TranDate).HasColumnType("datetime");
+            entity.Property(e => e.TranId)
+                .HasMaxLength(13)
+                .IsUnicode(false)
+                .HasColumnName("TranID");
+            entity.Property(e => e.TranNo)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.TranNoChngOrd)
+                .HasMaxLength(14)
+                .IsUnicode(false);
+            entity.Property(e => e.TranNoRel)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.TranNoRelChngOrd)
+                .HasMaxLength(19)
+                .IsUnicode(false);
+            entity.Property(e => e.UpdateDate).HasColumnType("datetime");
+            entity.Property(e => e.UpdateUserId)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("UpdateUserID");
+            entity.Property(e => e.UserFld1)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.UserFld2)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.UserFld3)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.UserFld4)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.VatinvoiceNumber)
+                .HasMaxLength(15)
+                .IsUnicode(false)
+                .HasColumnName("VATInvoiceNumber");
+            entity.Property(e => e.Vatnumber)
+                .HasMaxLength(13)
+                .IsUnicode(false)
+                .HasColumnName("VATNumber");
+            entity.Property(e => e.VattaxAmount)
+                .HasColumnType("decimal(15, 3)")
+                .HasColumnName("VATTaxAmount");
+            entity.Property(e => e.VattaxRate)
+                .HasColumnType("decimal(12, 6)")
+                .HasColumnName("VATTaxRate");
         });
 
         OnModelCreatingPartial(modelBuilder);
