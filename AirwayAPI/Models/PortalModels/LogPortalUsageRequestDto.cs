2f39e81a9668c2058347f65ffd00ddc51a2e3728
CCreech <ccreech@airway.com>
2025-03-19 13:32:10 -0400

    Directory layout adjustment

---
+++ AirwayAPI/Models/PortalModels/LogPortalUsageRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AirwayAPI.Models.PortalModels
+{
+    public class LogPortalUsageRequestDto
+    {
+        public string Url { get; set; }
+        public string Username { get; set; }
+    }
+}
