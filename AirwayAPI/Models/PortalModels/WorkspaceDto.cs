8750495c6ea6e46200732341c015a52bae3dd721
CCreech <ccreech@airway.com>
2025-03-19 15:11:01 -0400

    Create WorkspaceDto.cs

---
+++ AirwayAPI/Models/PortalModels/WorkspaceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AirwayAPI.Models.PortalModels
+{
+    public class WorkspaceDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+
+}
