ce70eedcac4d0ca4d6f7de341216059593376007
CCreech <ccreech@airway.com>
2025-03-19 13:33:27 -0400

    Create Portal DTO models

---
+++ AirwayAPI/Models/PortalModels/PortalMenuItemDto.cs
@@ -0,0 +1,14 @@
+﻿namespace AirwayAPI.Models.PortalModels
+{
+    public class PortalMenuItemDto
+    {
+        public int Id { get; set; }
+        public string Label { get; set; } = null!;
+        public string? IconName { get; set; }
+        public string? Path { get; set; }
+        public string ItemType { get; set; } = null!;
+        public int Ordering { get; set; }
+        public int ColumnGroup { get; set; }
+        public List<PortalMenuItemDto> Children { get; set; } = [];
+    }
+}
---
+++ AirwayAPI/Models/PortalModels/PortalRouteDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AirwayAPI.Models.PortalModels
+{
+    public class PortalRouteDto
+    {
+        public int Id { get; set; }
+        public string Path { get; set; } = null!;
+        public string ComponentName { get; set; } = null!;
+        public bool IsPrivate { get; set; }
+        public int Ordering { get; set; }
+    }
+}
