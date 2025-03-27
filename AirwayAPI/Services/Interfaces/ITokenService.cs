80fc4de8a6729bda717db97e422eb4a5618acae8
CCreech <ccreech@airway.com>
2025-03-27 09:15:46 -0400

    Update ITokenService.cs

--- AirwayAPI/Services/Interfaces/ITokenService.cs
+++ AirwayAPI/Services/Interfaces/ITokenService.cs
@@ -1,10 +1,12 @@
 ﻿using System.Security.Claims;
 
-namespace AirwayAPI.Services.Interfaces
+namespace AirwayAPI.Services.Interfaces;
+
+public interface ITokenService
 {
-    public interface ITokenService
-    {
-        string GenerateJwtToken(string username);
-        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
-    }
+    string GenerateJwtToken(string username);
+    Task<string> GenerateRefreshTokenAsync(string username);
+    Task<bool> ValidateRefreshTokenAsync(string token);
+    Task RevokeRefreshTokenAsync(string token);
+    ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
 }
