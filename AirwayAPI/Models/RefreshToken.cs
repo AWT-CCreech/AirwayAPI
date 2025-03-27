1d3d1d82c4b5ab4e51b328e4a92bd59f32f639e7
CCreech <ccreech@airway.com>
2025-03-27 09:15:38 -0400

    Create RefreshToken.cs

---
+++ AirwayAPI/Models/RefreshToken.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirwayAPI.Models;
+
+public partial class RefreshToken
+{
+    public int Id { get; set; }
+
+    public string Token { get; set; } = null!;
+
+    public string Username { get; set; } = null!;
+
+    public DateTime ExpiresAt { get; set; }
+
+    public bool IsRevoked { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
