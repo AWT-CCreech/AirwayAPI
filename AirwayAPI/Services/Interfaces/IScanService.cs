7f2e73da2f19c0f0a22b1c1ebef24546d2963e05
CCreech <ccreech@airway.com>
2025-04-11 16:16:29 -0400

    Create IScanService.cs

---
+++ AirwayAPI/Services/Interfaces/IScanService.cs
@@ -0,0 +1,14 @@
+﻿using AirwayAPI.Models;
+using AirwayAPI.Models.ScanHistoryModels;
+
+namespace AirwayAPI.Services.Interfaces
+{
+    public interface IScanService
+    {
+        Task<IEnumerable<ScanHistory>> SearchScanHistoryAsync(SearchScansDto searchDto);
+        Task<int> DeleteScansAsync(IEnumerable<int> selectedIds);
+        Task<int> UpdateScansAsync(IEnumerable<UpdateScanDto> updateDtos);
+        Task<int> AddTestLabScansAsync(IEnumerable<int> selectedIds);
+        Task<int> CopyScansAsync(CopyScansDto copyRequest);
+    }
+}
