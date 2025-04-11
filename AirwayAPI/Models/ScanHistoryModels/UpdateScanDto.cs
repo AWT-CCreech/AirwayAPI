5f44c01b9c2536308e7b23a46d352e7d69169fd8
CCreech <ccreech@airway.com>
2025-04-11 16:16:36 -0400

    Create UpdateScanDto.cs

---
+++ AirwayAPI/Models/ScanHistoryModels/UpdateScanDto.cs
@@ -0,0 +1,18 @@
+﻿namespace AirwayAPI.Models.ScanHistoryModels
+{
+    public class UpdateScanDto
+    {
+        public int RowId { get; set; }
+        public DateTime? ScanDate { get; set; }
+        public string? UserName { get; set; }
+        /// <summary>
+        /// This value indicates the order type (SO, PO, RMA, RTV/C). Your update logic can decide which order number to change.
+        /// </summary>
+        public string? OrderType { get; set; }
+        public string? OrderNum { get; set; }
+        public string? PartNo { get; set; }
+        public string? SerialNo { get; set; }
+        public string? HeciCode { get; set; }
+        // Add additional fields as needed.
+    }
+}
