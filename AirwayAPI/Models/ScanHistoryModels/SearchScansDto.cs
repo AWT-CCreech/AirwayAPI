a7d2e4da6614b7e9c8f4fd66da08ae7c2d176d6c
CCreech <ccreech@airway.com>
2025-04-21 10:55:16 -0400

    Update SearchScansDto.cs

--- AirwayAPI/Models/ScanHistoryModels/SearchScansDto.cs
+++ AirwayAPI/Models/ScanHistoryModels/SearchScansDto.cs
@@ -1,20 +1,16 @@
-﻿namespace AirwayAPI.Models.ScanHistoryModels;
-public class SearchScansDto
+﻿namespace AirwayAPI.Models.ScanHistoryModels
 {
-    // Default to one year ago
-    public DateTime ScanDateRangeStart { get; set; } = DateTime.Today.AddYears(-1);
-    // Default to today
-    public DateTime ScanDateRangeEnd { get; set; } = DateTime.Today;
-
-    public string SoNo { get; set; } = string.Empty;
-    public string PoNo { get; set; } = string.Empty;
-    public string Rmano { get; set; } = string.Empty;
-    public int? RTVID { get; set; }
-    public string PartNo { get; set; } = string.Empty;
-    public string SerialNo { get; set; } = string.Empty;
-    public string SNField { get; set; } = "SerialNo";
-    public string MNSCo { get; set; } = string.Empty;
-    public string ScanUser { get; set; } = string.Empty;
-    public string OrderType { get; set; } = string.Empty;
-    public int Limit { get; set; } = 1000;
+    public class SearchScansDto
+    {
+        public DateTime ScanDateRangeStart { get; set; } = DateTime.Today.AddYears(-1);
+        public DateTime ScanDateRangeEnd { get; set; } = DateTime.Today;
+        public string OrderNum { get; set; } = string.Empty;
+        public string OrderType { get; set; } = string.Empty;
+        public string PartNo { get; set; } = string.Empty;
+        public string SerialNo { get; set; } = string.Empty;
+        public string SNField { get; set; } = "SerialNo";
+        public string MNSCo { get; set; } = string.Empty;
+        public string ScanUser { get; set; } = string.Empty;
+        public int Limit { get; set; } = 1000;
+    }
 }
