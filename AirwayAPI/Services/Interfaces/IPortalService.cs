14ed7745251ff5b59dafeb48495e3f57e5259f29
CCreech <ccreech@airway.com>
2025-04-24 15:02:37 -0400

    Create IPortalService.cs

---
+++ AirwayAPI/Services/Interfaces/IPortalService.cs
@@ -0,0 +1,13 @@
+﻿using AirwayAPI.Models.PortalModels;
+
+namespace AirwayAPI.Services.Interfaces
+{
+    public interface IPortalService
+    {
+        Task<List<WorkspaceDto>> GetWorkspacesAsync();
+        Task<List<PortalMenuItemDto>> GetMenuAsync(int workspaceId, int userId);
+        Task AddFavoriteAsync(int userId, int itemId);
+        Task RemoveFavoriteAsync(int userId, int itemId);
+        Task<List<PortalRouteDto>> GetRoutesAsync(int workspaceId);
+    }
+}
