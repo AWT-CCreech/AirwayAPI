18306970a84e3c2551fa05dd1e77a9d335424ee5
CCreech <ccreech@airway.com>
2025-04-25 13:25:38 -0400

    Create JwtSettings.cs

---
+++ AirwayAPI/Configuration/JwtSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace AirwayAPI.Configuration
+{
+    public class JwtSettings
+    {
+        public required string Key { get; init; }
+        public required string Issuer { get; init; }
+        public required string Audience { get; init; }
+        public int TokenLifetimeMinutes { get; init; } = 30;
+        public int RefreshLifetimeDays { get; init; } = 14;
+    }
+}
